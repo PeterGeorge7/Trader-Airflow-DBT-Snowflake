1db5fa65a8369a78b7ce486b61a4801fdfc3b56c
PeterGeorge7 <peto.george.3@gmail.com>
2026-04-19 22:44:58 +0200

    Finished bank api ingestion and save it into snowflake

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,2 @@
 FROM astrocrpublic.azurecr.io/runtime:3.2-2
+# FROM quay.io/astronomer/astro-runtime:11.3.0
--- dags/bank_ingestion.py
+++ dags/bank_ingestion.py
@@ -1,10 +1,15 @@
 # bank ingestion DAG
 import requests
-from airflow.sdk import dag, task
+from airflow.sdk import dag, task, Asset
 from datetime import datetime, timedelta
 from airflow.sensors.base import PokeReturnValue
+from pathlib import Path
 import time
+import tempfile
 import pandas as pd
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+# csv_bank_data = Dataset("data/world_bank_data.csv")
 
 countries_codes = {
     "Egypt": "EGY",
@@ -24,8 +29,8 @@ indicators = {
 
 
 @dag(
-    dag_id="bankapi_ingestion",
-    schedule=timedelta(years=1),
+    dag_id="bank_ingestion",
+    schedule=timedelta(days=365),
     start_date=datetime(2026, 1, 1),
     catchup=False,
 )
@@ -38,6 +43,21 @@ def bank_ingestion():
             )
             return PokeReturnValue(is_done=True, xcom_value="API is online")
         except requests.exceptions.RequestException:
+            return PokeReturnValue(is_done=False)
+
+    snowflake_hook = SnowflakeHook(snowflake_conn_id="snowflake_conn")
+
+    @task.sensor(poke_interval=60, timeout=3600)
+    def check_snowflake_connection():
+        try:
+            conn = snowflake_hook.get_conn()
+            conn.cursor().execute("SELECT 1")
+            print("Snowflake connection successful.")
+            conn.close()
+            return PokeReturnValue(is_done=True, xcom_value="Snowflake is online")
+
+        except Exception as e:
+            print(f"Snowflake connection failed: {e}")
             return PokeReturnValue(is_done=False)
 
     # that will .expand()
@@ -73,26 +93,91 @@ def bank_ingestion():
             except requests.exceptions.RequestException as e:
                 print(f"Failed to fetch {ind_name} for {country_name}: {e}")
 
-    @task()
-    def ingest_data(all_extracted_data):
+        return all_data
+
+    # check snowflake connection
+
+    @task(outlets=[Asset("snowflake://world_bank_raw")])
+    def save_to_snowflake(all_extracted_data):
         full_data = []
         for c_list in all_extracted_data:
 
-            for ic_dict in c_list:
-                full_data.append(ic_dict)
+            if c_list:
+                full_data.extend(c_list)
 
-        df = pd.DataFrame(full_data)
-        df.to_csv("data/world_bank_data.csv", index=False)
+        if not full_data:
+            print("No extracted data to load into Snowflake.")
+            return
+
+        # df = pd.DataFrame(full_data)
+        # create df columns in the order of the Snowflake table
+        df = pd.DataFrame(
+            full_data,
+            columns=[
+                "country_code",
+                "country_name",
+                "indicator_code",
+                "indicator_name",
+                "year",
+                "value",
+                "date_retrieved",
+            ],
+        )
+
+        conn = snowflake_hook.get_conn()
+        cursor = conn.cursor()
+        temp_csv_path = None
+
+        try:
+            with tempfile.NamedTemporaryFile(
+                mode="w",
+                suffix=".csv",
+                newline="",
+                encoding="utf-8",
+                delete=False,
+            ) as temp_file:
+                temp_csv_path = temp_file.name
+                df.to_csv(temp_file, index=False)
+
+            cursor.execute(
+                f"PUT file://{temp_csv_path} @my_int_stage AUTO_COMPRESS=TRUE OVERWRITE=TRUE"
+            )
+            cursor.execute(
+                f"""
+                COPY INTO world_bank_raw
+                FROM @my_int_stage/{Path(temp_csv_path).name}.gz
+                """
+            )
+
+            print(f"Successfully loaded rows into Snowflake.")
+        finally:
+            cursor.close()
+            conn.close()
+
+            if temp_csv_path:
+                Path(temp_csv_path).unlink(missing_ok=True)
+
+    # @task(outlets=[csv_bank_data])
+    # def save_csv(all_extracted_data):
+    #     full_data = []
+    #     for c_list in all_extracted_data:
+
+    #         for ic_dict in c_list:
+    #             full_data.append(ic_dict)
+
+    #     df = pd.DataFrame(full_data)
+    #     df.to_csv("data/world_bank_data.csv", index=False)
 
     api_check = check_api_availability()
+    snowflake_check = check_snowflake_connection()
 
     country_codes_list = list(countries_codes.items())
 
     extracted_data = extract_country_data.expand(country_tuple=country_codes_list)
 
-    api_check >> extracted_data
+    [api_check, snowflake_check] >> extracted_data
 
-    saved_file_path = ingest_data(extracted_data)
+    snowflake_save = save_to_snowflake(extracted_data)
 
 
 bank_ingestion()
